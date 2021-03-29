8b0a31c0c400a951fa84f03b026bbc2c819f6bfb
alebarreto <alessandrobarreto50@gmail.com>
2021-03-29 14:13:39 -0400

    chore: fix saveState in viewModel

--- app/src/main/java/com/barreto/unsplashapi/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/barreto/unsplashapi/ui/gallery/GalleryViewModel.kt
@@ -1,7 +1,7 @@
 package com.barreto.unsplashapi.ui.gallery
 
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -11,15 +11,18 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class GalleryViewModel @Inject constructor(private val repository: UnSplashRepository) :
+class GalleryViewModel @Inject constructor(
+    private val repository: UnSplashRepository, state: SavedStateHandle
+) :
     ViewModel() {
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
 
-
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    //    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
